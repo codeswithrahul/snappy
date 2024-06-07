b94ad744e87ff75e2c914271137aa8a67153aa5b
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2024-06-07 11:51:44 +0530

    vite setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <main className="flex h-screen border">
+        {/* ------------- Private Routes --------- */}
+
+
+        {/* ------------ Public Routes ----------- */}
+      </main>
+    </>
+  );
+}
+
+export default App;
